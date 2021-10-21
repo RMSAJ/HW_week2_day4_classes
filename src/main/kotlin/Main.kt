6dc20ac52dc885acd412ddaab4a8df27aa4641cc
haya <haya.saud.ruh.k@tuwaiq.edu.sa>
2021-10-21 08:53:56 +0300

    class 20OCT

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,47 @@
+
 fun main(args: Array<String>) {
 
-    // write your code here
+    var cors = Course()
+    var prof= profissor()
 
+}
+
+class Course {
+    var CourseName:String""
+    var number_of_lectures = Int
+    var students:String=""
+    var professor:String=""
+    // the idea in my mind doing list for each "coursName then when i add mor student to the cours1 i will check if the student mor than or equal 20 here i can't add student then else i will add a new student
+    // i have many idea but i can't write it on the cod :(
+
+}
+
+
+class Students {
+
+    var firstName = "Haya"
+    var lastName = "saud"
+    var courses:String=""
+
+    public  fun fullName() // return “firstName lastName”
+    {
+        println("$firstName" , "$lastName")
+    }
+
+}
+
+public class profissor {
+
+    var firstName = "Haya"
+    var lastName = "saud"
+    var telephone = Int
+    var courses :String=""
+
+    public  fun fullName() // return “firstName lastName”
+    {
+        println("$firstName", "$lastName")
+        println("telephone:","$telephone")
+        println("Courses","$courses")
+    }
 
 }
